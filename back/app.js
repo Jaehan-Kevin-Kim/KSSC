7eadfbee6337c1cbca67af8986e3c3c0eb06b17f
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-05-15 17:43:02 -0600

    Updated front-end design

--- back/app.js
+++ back/app.js
@@ -1,7 +1,16 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const postRouter = require("./routes/post");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
 app.get("/", (req, res) => {
   res.send("main page");
 });
@@ -10,6 +19,10 @@ app.get("/go", (req, res) => {
   res.send("go page");
 });
 
+app.post("/user/login", (req, res) => {
+  console.log(res);
+  return res.status(200).json(req.body);
+});
 // app.post('')
 
 app.use("/post", postRouter);
